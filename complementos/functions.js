7ff47c3c99630c28bb64b1d0a1222c8eecb8cd9e
Lucasgomez14 <93557340+Lucasgomez14@users.noreply.github.com>
2023-06-04 12:29:27 -0300

    CV actualizado al día 04-06-2023

---
+++ complementos/functions.js
@@ -0,0 +1,16 @@
+let elementosAcordeon = document.getElementsByClassName("acordeon");
+
+for (let i = 0; i< elementosAcordeon.length; i++)
+{
+    elementosAcordeon[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display == "block")
+        {
+            panel.style.display = "none";
+        }
+        else {
+            panel.style.display = "block";
+        }
+    });
+}
